ea30d8e1094acbffa32f7c10353cd5547607fade
t480s <abcd0425@gmail.com>
2023-10-05 11:03:46 +0800

    a trial for payment

--- static/js/script_booking.js
+++ static/js/script_booking.js
@@ -113,3 +113,156 @@ document.querySelector('.booking_delete').addEventListener('click', () => {
         console.error('Booking Fetch Error:', bookingError);
     });
   }
+
+src="https://js.tappaysdk.com/sdk/tpdirect/v5.17.0"
+TPDirect.setupSDK(APP_ID, 'APP_KEY', 'sandbox')
+
+TPDirect.card.setup({
+    // Display ccv field
+    let fields = {
+        number: {
+            // css selector
+            element: '#card-number',
+            placeholder: '**** **** **** ****'
+        },
+        expirationDate: {
+            // DOM object
+            element: document.getElementById('card-expiration-date'),
+            placeholder: 'MM / YY'
+        },
+        ccv: {
+            element: '#card-ccv',
+            placeholder: 'ccv'
+        }
+    }
+    // Not display ccv field
+    // let fields = {
+    //     number: {
+    //         // css selector
+    //         element: '#card-number',
+    //         placeholder: '**** **** **** ****'
+    //     },
+    //     expirationDate: {
+    //         // DOM object
+    //         element: document.getElementById('card-expiration-date'),
+    //         placeholder: 'MM / YY'
+    //     }
+    // }
+
+    fields: fields,
+    styles: {
+        // Style all elements
+        'input': {
+            'color': 'gray'
+        },
+        // Styling ccv field
+        'input.ccv': {
+            // 'font-size': '16px'
+        },
+        // Styling expiration-date field
+        'input.expiration-date': {
+            // 'font-size': '16px'
+        },
+        // Styling card-number field
+        'input.card-number': {
+            // 'font-size': '16px'
+        },
+        // style focus state
+        ':focus': {
+            // 'color': 'black'
+        },
+        // style valid state
+        '.valid': {
+            'color': 'green'
+        },
+        // style invalid state
+        '.invalid': {
+            'color': 'red'
+        },
+        // Media queries
+        // Note that these apply to the iframe, not the root window.
+        '@media screen and (max-width: 400px)': {
+            'input': {
+                'color': 'orange'
+            }
+        }
+    },
+    // if the card number is correct, it would show the last 6 digit number.
+    isMaskCreditCardNumber: true,
+    maskCreditCardNumberRange: {
+        beginIndex: 6,
+        endIndex: 11
+    }
+})
+
+
+TPDirect.card.onUpdate(function (update) {
+    // update.canGetPrime === true
+    // --> you can call TPDirect.card.getPrime()
+    if (update.canGetPrime) {
+        // Enable submit Button to get prime.
+        // submitButton.removeAttribute('disabled')
+    } else {
+        // Disable submit Button to get prime.
+        // submitButton.setAttribute('disabled', true)
+    }
+
+    // cardTypes = ['mastercard', 'visa', 'jcb', 'amex', 'unionpay','unknown']
+    if (update.cardType === 'visa') {
+        // Handle card type visa.
+    }
+
+    // if number field is wrong 
+    if (update.status.number === 2) {
+        // setNumberFormGroupToError()
+    } else if (update.status.number === 0) {
+        // setNumberFormGroupToSuccess()
+    } else {
+        // setNumberFormGroupToNormal()
+    }
+
+    if (update.status.expiry === 2) {
+        // setNumberFormGroupToError()
+    } else if (update.status.expiry === 0) {
+        // setNumberFormGroupToSuccess()
+    } else {
+        // setNumberFormGroupToNormal()
+    }
+
+    if (update.status.ccv === 2) {
+        // setNumberFormGroupToError()
+    } else if (update.status.ccv === 0) {
+        // setNumberFormGroupToSuccess()
+    } else {
+        // setNumberFormGroupToNormal()
+    }
+})
+
+
+// call TPDirect.card.getPrime when user submit form to get tappay prime
+// $('form').on('submit', onSubmit)
+
+function onSubmit(event) {
+    event.preventDefault()
+
+    // get the status of TapPay Fields
+    const tappayStatus = TPDirect.card.getTappayFieldsStatus()
+
+    // chekc if it is good to getPrime
+    if (tappayStatus.canGetPrime === false) {
+        alert('can not get prime')
+        return
+    }
+
+    // Get prime
+    TPDirect.card.getPrime((result) => {
+        if (result.status !== 0) {
+            alert('get prime error ' + result.msg)
+            return
+        }
+        alert('get prime 成功，prime: ' + result.card.prime)
+
+        // send prime to your server, to pay with Pay by Prime API .
+        // Pay By Prime Docs: https://docs.tappaysdk.com/tutorial/zh/back.html#pay-by-prime-api
+    })
+}
